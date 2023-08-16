c4160c7215917ba825f4af71a1c5c60a0648cfac
Rich Dobbs <rich.dobbs.13440@gmail.com>
2023-08-16 15:14:22 -0600

    Initial setup after running cookie cutter.

---
+++ chromatofore-octoprint-plugin/octoprint_chromatofore/static/js/chromatofore.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Chromatofore
+ *
+ * Author: Rich Dobbs
+ * License: AGPLv3
+ */
+$(function() {
+    function ChromatoforeViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: ChromatoforeViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_chromatofore, #tab_plugin_chromatofore, ...
+        elements: [ /* ... */ ]
+    });
+});
