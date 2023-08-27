07507cde8b6e0985a4d0dfd6dd8d2552bed2394b
Rich Dobbs <rich.dobbs.13440@gmail.com>
2023-08-26 20:35:40 -0600

    Still trying to get some settings to work

--- octoprint_chromatofore/static/js/chromatofore.js
+++ octoprint_chromatofore/static/js/chromatofore.js
@@ -1,52 +1,35 @@
 // octoprint_chromatofore/static/js/chromatofore.js
 
-console.log("Entering chromatofore.js");
+$(function() {
 
-function ChromatoforeViewModel(parameters) {
+    function ChromatoforeViewModel(parameters) {
 
-    try {
-        console.log("Initializing iii ChromatoforeViewModel");
-        console.log(parameters);
-        console.log("Post printing parameters");
-        // console.log(parameters[0].settings);
-        // console.log(parameters[0].settings.plugins);
-        // console.log("Post printing plugins");
-    } catch (error) {
-        console.error("Error during ChromatoforeViewModel instantiation:", error);
-    }
-    var self = this;
+        let self = this;
 
-    self.settings = parameters[0].settings;
-    try {
-        console.log("self.settings");
-        console.log(self.settings);
-        console.log("Post printing self.settings");
-    } catch (error) {
-        console.error("Error during plugins logging", error);
-    }
+        self.settings = parameters[0];
 
-    self.gpio_boards = ko.observableArray(self.settings.plugins.chromatofore.gpio_boards);
+
+        // Function to log the settings
+        self.logSettings = function() {
+            console.log(self.settings);
+            
+            // Assuming your plugin's identifier in Python is "your_plugin_name"
+            console.log(self.settings.plugins.your_plugin_name);
+        };
+
+        self.logSettings();
+
     
-    // Add a board
-    self.addGpioBoard = function() {
-        self.gpio_boards.push("");
-    };
+    }
 
-    // Remove a board
-    self.removeGpioBoard = function(board) {
-        self.gpio_boards.remove(board);
-    };
+    // Register the ViewModel
+    OCTOPRINT_VIEWMODELS.push({ 
+        construct: ChromatoforeViewModel,
+        dependencies: ["settingsViewModel"],
+        elements: ["#settings_plugin_chromatofore"]
+    }); 
 
-   
-}
 
-// Register the ViewModel
-OCTOPRINT_VIEWMODELS.push([
-    ChromatoforeViewModel,
-    ["settingsViewModel"],
-    "#settings_plugin_chromatofore"
-]); 
-
-console.log("Registered the ViewModel");
+});
 
 
