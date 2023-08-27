5d0a2f76f993de65dab9858651eb21258f383734
Rich Dobbs <rich.dobbs.13440@gmail.com>
2023-08-26 22:25:55 -0600

    Still trying to get some settings to work

--- octoprint_chromatofore/static/js/chromatofore.js
+++ octoprint_chromatofore/static/js/chromatofore.js
@@ -14,9 +14,9 @@ $(function() {
         self.actuators = ko.observableArray([]);
 
         // Load initial data from settings
-        if(parameters[0] && parameters[0].settings && parameters[0].settings.plugins && parameters[0].settings.plugins.chromatofore && parameters[0].settings.plugins.chromatofore.gpio_boards) {
-            self.gpio_boards(parameters[0].settings.plugins.chromatofore.gpio_boards());
-        }
+        // if(parameters[0] && parameters[0].settings && parameters[0].settings.plugins && parameters[0].settings.plugins.chromatofore && parameters[0].settings.plugins.chromatofore.gpio_boards) {
+        //     self.gpio_boards(parameters[0].settings.plugins.chromatofore.gpio_boards());
+        // }
 
         // Operations
         self.addGpioBoard = function() {
@@ -26,6 +26,15 @@ $(function() {
         self.removeGpioBoard = function(board) {
             self.gpio_boards.remove(board);
         };        
+
+        // This will get called before the ChromatoforeViewModel gets bound to the DOM, but after its
+        // dependencies have already been initialized. It is especially guaranteed that this method
+        // gets called _after_ the settings have been retrieved from the OctoPrint backend and thus
+        // the SettingsViewModel been properly populated.
+        self.onBeforeBinding = function() {
+            var pluginSettings = self.settingsViewModel.settings.plugins.chromatofore;
+            self.gpio_boards(pluginSettings.gpio_boards);
+        }        
         
 
         // // This will be executed once OctoPrint initializes the view model
