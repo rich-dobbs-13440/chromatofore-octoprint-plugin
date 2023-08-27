ecfc570389211980be9f700b879a5d3b5b9b69be
Rich Dobbs <rich.dobbs.13440@gmail.com>
2023-08-27 13:56:55 -0600

    More logging for simple api command handling

--- octoprint_chromatofore/static/js/chromatofore.js
+++ octoprint_chromatofore/static/js/chromatofore.js
@@ -67,6 +67,7 @@ $(function() {
         self.validateGpioBoard = function(address) {
             // This should make an AJAX request to your plugin's backend to verify the address.
             OctoPrint.simpleApiCommand("chromatofore", "validate_i2c", { address: address }).done(function(response) {
+                console.log("Got response from simpleApiCommand");
                 if (response.valid) {
                     alert('Address ' + address + ' is valid!');
                 } else {
