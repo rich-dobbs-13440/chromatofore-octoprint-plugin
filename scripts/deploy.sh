7b1dd7b43b08fbbdb5947aba290081d51ed43531
Rich Dobbs <rich.dobbs.13440@gmail.com>
2023-08-28 13:29:55 -0600

    Better logic for deploy.sh - run from an directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+set -e  # Stop script on error
+set -x  # Echo commands
+
+# Get the directory where the script is located
+script_dir=$(dirname "$0")
+version_file="$script_dir/../version.txt"
 
 # Read current version from version.txt
-current_version=$(cat "../version.txt")
+current_version=$(cat $version_file)
+echo "Current Version:" $current_version
 
 # Increment the patch version number
 IFS='.' read -ra version_parts <<< "$current_version"
@@ -11,7 +19,7 @@ new_patch=$((patch + 1))
 new_version="$major.$minor.$new_patch"
 
 # Update version.txt with new version
-echo "$new_version" > "../version.txt"
+echo "$new_version" > $version_file
 
 rsync -avz ~/code/chromatofore-octoprint-plugin/ rld@chromatofore.local:~/chromatofore-octoprint-plugin/
 ssh rld@chromatofore.local "~/oprint/bin/pip install ~/chromatofore-octoprint-plugin"
